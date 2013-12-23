e0760b37b16d0045d7de5de62b14bb74a6ed3494
Rob Smith <kormoc@gmail.com>
2013-12-22 23:02:22 -0800

    Also provide a filter to use in your rendered _layout rather then as a per page generator.

--- gpg_clearsign.rb
+++ gpg_clearsign.rb
@@ -12,27 +12,62 @@ require 'GPGME'
 module GPGClearsign
   class GPGClearsignGenerator < Jekyll::Generator
     def generate(site)
-      if site.config.has_key?("GPGClearsign") and !site.config["GPGClearsign"]
+      if !site.config.has_key?("GPGClearsign") or !site.config["GPGClearsign"]
         return
       end
-      
+
       site.pages.each { |p| clearsign p }
       site.posts.each { |p| clearsign p }
     end
-    
+
     private
     def clearsign(obj)
+      if not obj.url =~ /^\.html$/i
+        return
+      end
+
       if obj.data.has_key?("GPGClearsign") and !obj.data["GPGClearsign"]
         return
       end
-      
+
+      # Non-rendered liquid markup
+      if obj.content.include? '{{'
+        return
+      end
+
+      # Non-rendered liquid tags
+      if obj.content.include? '{%'
+        return
+      end
+
       crypto = GPGME::Crypto.new
       obj.content.prepend("\n-->\n")
       obj.content << "\n<!--\n"
       obj.content = (crypto.clearsign obj.content).to_s
       obj.content.prepend("\n<!--\n")
       obj.content << "\n-->\n"
-      return
     end
   end
 end
+
+module Jekyll
+  module GPGClearsignFilter
+    def gpg_clearsign(content)
+
+      # Already signed?
+      if content.include? '-----BEGIN PGP SIGNED MESSAGE-----'
+        return content
+      end
+
+      content.prepend("\n                    \n-->\n")
+      content << "\n<!--\n                    \n"
+      content = (GPGME::Crypto.new.clearsign content).to_s
+      content.prepend("\n<!--\n                    \n")
+      content << "\n                    \n-->\n"
+
+      return content
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::GPGClearsignFilter)
